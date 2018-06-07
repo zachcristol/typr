d4a391292f25c17d05cc02ca7136bf94de8838dd
ezrabartlett <ezrabartlett@tamu.edu>
2018-06-06 22:45:36 -0500

    Added split text, so that words dissappear on the right as the user types. Also, formatting.

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
 
 
 
-let growingString = ""
+let growingString = "";
+
+// String that the user will have to type
+let shrinkingString  = "Lorum Ipsum";
+
+// Holds the typed characters so we can put them back later
+let shrinkingStack = [];
+
+let totalChars = 0;
+
+// Things that need to be done when the page loads
+window.onload = function() {
+    document.getElementById("typrRight").innerHTML = shrinkingString;
+};
 
 document.addEventListener('keypress', (event) => {
-    // Adds non enter character to string, and writes that string to the div.
+    // Adds non enter character to string, and writes that string to the div. Adds one to totalChars
     if (event.charCode != 13) {
+        totalChars++;
         growingString += event.key;
-        document.getElementById("typr").innerHTML = growingString;
+
+        // Deal with the text on the right side
+        shrinkingStack.push(shrinkingString[0]);
+        shrinkingString = shrinkingString.substring(1, shrinkingString.length);
+
+        document.getElementById("typrLeft").innerHTML = growingString;
+        document.getElementById("typrRight").innerHTML = shrinkingString;
     }
   });
 
 document.addEventListener('keydown', (event) => {
-    // Removes multiple backspaces
+    // Removes multiple backspaces, and subtracts from the characters typed
     if (event.key == "Backspace") {
-        growingString = growingString.substring(0, growingString.length - 1);
-        document.getElementById("typr").innerHTML = growingString;
+        totalChars--;
+
+        growingString   = growingString.substring(0, growingString.length - 1);
+
+        // If there are characters in the stack, move them to the right.
+        if(shrinkingStack.length != 0){
+            shrinkingString = shrinkingStack.pop()+shrinkingString;
+        }
+
+        document.getElementById("typrLeft").innerHTML  = growingString;
+        document.getElementById("typrRight").innerHTML = shrinkingString;
     }
 });
